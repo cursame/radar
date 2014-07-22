5951731ae7837d45b0ee8cf192f4a7f4674c7f9a
alfredo777 <jose_alfredo@cursa.me>
2014-07-22 11:42:08 -0500

    filtro de administración

--- app/controllers/management_controller.rb
+++ app/controllers/management_controller.rb
@@ -1,5 +1,6 @@
 class ManagementController < ApplicationController
 layout 'admin'
+  before_filter :filter_session
   def applications
   	 @users = User.where(confirmation: false).paginate(:page => params[:page], :per_page => 30)
   end
@@ -16,7 +17,14 @@ layout 'admin'
   	 @users = User.where(confirmation: true).paginate(:page => params[:page], :per_page => 30)
   end
 private
-  def filter
-
+  ########### filtro de acceso al administrador #############
+  def filter_session
+    if session[:user] == nil
+      redirect_to root_path
+       else
+        unless current_user.admin_attributes
+          redirect_to user_path(current_user.id)
+        end
+    end
   end
 end
