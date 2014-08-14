45c8d456c22822a9b530c0320569afffa3dcdf6f
alfredo777 <jose_alfredo@cursa.me>
2014-08-14 12:43:57 -0500

    cambbios en aplicación de form

--- app/controllers/red_lights_controller.rb
+++ app/controllers/red_lights_controller.rb
@@ -81,7 +81,7 @@ class RedLightsController < ApplicationController
          Denuncia casos de violencia escolar
       </a>"
 
-    if  parce_url(@institution.url, @origin, params[:pathx] )
+    if  parce_url(@institution.url, @origin, params[:pathx], params[:subdomain] )
     render :json =>  @iframe.to_json
      else
     render :json =>  @nots.to_json
@@ -135,9 +135,9 @@ class RedLightsController < ApplicationController
       redirect_to root_path
     end
   end
-  def parce_url(url, acces, pathx)
+  def parce_url(url, acces, pathx='/', subdomain='')
     ######### parce url #########
-    removesimplex = ['http', 'https', '/', 'www', ':', '.', "#{pathx}"]
+    removesimplex = ['http', 'https', '/', "#{subdomain}", ':', '.', "#{pathx}"]
 
     remove = removesimplex
     puts "#{remove}"
