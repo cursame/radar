79a30f985bdbdffb8bba206bafdc95c5d3685c32
alfredo777 <jose_alfredo@cursa.me>
2014-08-29 15:39:43 -0500

    appi

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,17 +1,32 @@
 class ApiController < ApplicationController
   def documentation_methods
   end
-
+  
+  ############################### REGISTRO DE USUARIO ######################
   def register_by_api
-    
+    # http://localhost:3000/api/register_by_api?
+    user = User.find_by_email(params[:email])
+    if user == nil
+      user = create_user_for_api(params[:name], params[:email], params[:charge], params[:password], params[:password] , params[:terms_of_service], params[:confirmation], params[:insitution], params[:url])
+    else
+      user = 'The user already exists (there is a registered user with this email)'
+    end
+   render :json => user.to_json
   end
-
-  def create_user_for_api(name, email, charge, password, terms_of_service)
+  ############################# CREACION DEL USUARIO POR API ######################
+  def create_user_for_api(name, email, charge, password, password_confirmation,  terms_of_service, confirmation)
+    @u = User.create(name: name, email: email, charge: charge, password: password, password_confirmation: password_confirmation, confirmation: confirmation, terms_of_service: terms_of_service)
+    @url = create_institution_and_user(name, url, @u.id)
+    createdu = @u + @url 
   end
-
+  ############################# CREACION DE INSTITUCION POR API ###################
   def create_institution_and_user(name, url, user_id)
+    @intitution = Institution.create(name: name, url: url, user_id: user.id)
   end
+  def requests_by_token 
 
+  end
+  ############################ ACCESO A LAS ALERTAS POR API #######################
   def acces_to_red_lights
   	#http://localhost:3000/api/acces_to_red_lights?email=alfredo@cursa.me&&token=7938c619b2f85202
   	red_lights = []
