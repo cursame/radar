cc872b6b515de85f83497a0bdbdb5427b8e38a3d
lruiz05 <luis05021990@gmail.com>
2015-10-23 16:38:10 -0500

    Adds the gender field to user model
    This is an enum field.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151023211637) do
+ActiveRecord::Schema.define(version: 20151023212328) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,9 +49,11 @@ ActiveRecord::Schema.define(version: 20151023211637) do
     t.datetime "updated_at",                             null: false
     t.string   "name",                   default: "",    null: false
     t.boolean  "accepted_terms",         default: false, null: false
+    t.integer  "gender",                 default: 2,     null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["gender"], name: "index_users_on_gender", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "users_roles", id: false, force: :cascade do |t|
