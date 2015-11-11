7f6766a54d1f3087dae50c71bf697c337abdfe62
lruiz05 <luis05021990@gmail.com>
2015-11-11 12:44:20 -0600

    Update factory Institution
    Adds the following fields:
    
     -address
     -state
     -municipality
     -phone
     -cct
     -education_level
     -category

--- spec/factories/institutions.rb
+++ spec/factories/institutions.rb
@@ -2,5 +2,12 @@ FactoryGirl.define do
   factory :institution do
     title { Faker::Name.name }
     subdomain { Faker::Internet.domain_word }
+    address { Faker::Address.street_address }
+    state 'Distrito Federal'
+    municipality 'Cuauhtemoc'
+    phone { Faker::PhoneNumber.phone_number }
+    cct '01DBP0002T'
+    education_level "básico"
+    category "técnica"
   end
 end
