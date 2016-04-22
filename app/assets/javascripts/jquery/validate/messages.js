454995eeeed3f29fa4c6048ea573f5f69c4e3e62
lruiz05 <luis05021990@gmail.com>
2016-04-22 16:15:17 -0500

    Update validate error messages file
    Adds messages for:
    
    * email
    * minlength
    * equalTo

--- app/assets/javascripts/jquery/validate/messages.js
+++ app/assets/javascripts/jquery/validate/messages.js
@@ -1,4 +1,6 @@
 jQuery.extend(jQuery.validator.messages, {
     required: "Completa este campo",
-    email: "Dirección de correo electrónico no válida"
+    email: "Dirección de correo electrónico no válida",
+    minlength: "Por favor, introduzca al menos 8 caracteres.",
+    equalTo: "Por favor, introduzca el mismo valor de nuevo"
 });
